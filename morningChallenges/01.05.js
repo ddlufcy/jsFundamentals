e8b122d7b2b1afc077fa0e05ae44482f0b0ff220
ddlufcy <ddlufcy@gmail.com>
2019-09-20 17:03:32 -0400

    add friday work

---
+++ morningChallenges/01.05.js
@@ -0,0 +1,19 @@
+let bladeRunner = {
+    nameOfMovie: 'Bladerunner',
+    runtime: 90,
+    characters: [{
+        {
+            name: 'Deckard',
+            age: 33,
+            items: [{item1: "gun"} {item2: "badge"}]
+        },
+        {
+            name: 'Rachel',
+            age: 2,
+            items: [{item1: "fur coat"}{item2: "cigarette"}]
+        }
+    };
+    genre: 'scifi'
+};
+
+console.log(`${bladeRunner.characters}`);
